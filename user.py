3782605b82539a1935b75b61c5407e3c2a938515
Simón Acosta <simonacostfc@gmail.com>
2025-12-03 11:01:59 -0500

    reinicio en user

--- user.py
+++ user.py
@@ -14,23 +14,4 @@ templates = Jinja2Templates(directory="Templates")
 async def show_create(request: Request):
     return templates.TemplateResponse("new_user.html", {"request": request})
 
-@router.post("/", response_model=User, status_code=201)
-async def create_user(request: Request,
-                      session: SessionDep,
-                      name: str = Form(...),
-                      year: int = Form(...),
-                      status: bool = Form(True),
-                      img: Optional[UploadFile] = File(None)
-                      ):
-    try:
-        new_user = UserCreate(name=name, year=year, status=status, img=img_url)
-
-        user = User.model_validate(new_user)
-        session.add(user)
-        await session.commit()
-        await session.refresh(user)
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-    return RedirectResponse(url=f"/users/{user.id}", status_code=302)
 
